5450c303947ea219177b7d7177571a61d3c908d9
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2023-07-11 10:27:47 +0200

    Jwt Service

--- src/main/java/com/example/security/config/JwtService.java
+++ src/main/java/com/example/security/config/JwtService.java
@@ -1,10 +1,38 @@
 package com.example.security.config;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "24f52ea8bcf9585592814a2d627a52ab245c76a8bf5feecf17412a35fdbe9e8d";
+
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token,Claims::getSubject);
+    }
+
+    private <T> T extractClaim (String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
